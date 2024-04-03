d247c988cec9c6af9db708009c77dfb0489f828f
Isha Dubey <ishadubey36@gmail.com>
2024-04-03 16:27:04 +0530

    Practicing

---
+++ Practice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+        // int a = 1;
+        // int b = 4;
+        // int c = 6;
+        // int sum = a + b + c;
+        // System.out.println(sum);
+
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter m : ");
+        int m = Sc.nextInt();
+        
+        Sc.close(); 
+    }    
+}
