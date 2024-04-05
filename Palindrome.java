723ceddd204026e15fc0e9833b41d2e7b33c26aa
Isha Dubey <ishadubey36@gmail.com>
2024-04-05 23:35:30 +0530

    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+public class Palindrome {
+
+    public static void main(String[] args) {
+        // int num = 12321;
+        // String string =  String.valueOf(num);
+        // String rev_String = "";
+        // for(int i = string.length() - 1; i >= 0; i--){
+        //     rev_String += string.charAt(i);
+            
+        // }
+      
+        // if (rev_String.equals(string)){
+        //     System.out.println("this is palindrome");
+    
+        // }else {
+        //     System.out.println("not a palindrome");
+        // }
+
+        for(int i=0; i<=4 ; i++){
+            String a = "*".repeat(i);
+          
+            System.out.println(a);
+        }
+    }
+}
